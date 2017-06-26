c1a871a473daf6b696ae12c55d34e049376b8275
Andrey Koval <koval.andrey@gmail.com>
2017-06-26 12:35:53 -0400

    tweak

--- manipulation/anonymizer.R
+++ manipulation/anonymizer.R
@@ -27,96 +27,31 @@ requireNamespace("DT", quietly=TRUE) # for dynamic tables
 # ---- declare-globals ---------------------------------------------------------
 # path_input_mapping  <- "./data-unshared/derived/dto_location_map.rds" # `./0-ellis-location-map.R`
 path_input   <- "./data-unshared/derived/dto_patient_events_addictions_4264.rds"
-path_save    <- "./data-public/derived/dto_patient_events_addictions_4264_anon"
+path_save    <- "./data-unshared/derived/dto_patient_events_addictions_4264_anon"
 testit::assert("File does not exist", base::file.exists(path_input))
 # ---- utility-functions ----------------------------------------------------- 
 # ---- load-data ---------------------------------------------------------------
 ds <- readRDS(path_input)
 # ---- inspect-data --------------------
-ds %>% dplyr::glimpse(70)
+ds %>% dplyr::glimpse()
 # ---- tweak-data ---------------------
 # ---- anonymize ------------------
-scramble_one <- function(ds, person_id){
-  # person_id = 10042753
-  ls_bucket <- list()
-  all_palette_codes <- setdiff(unique(ds$palette_code), NA)
-  for(pc in all_palette_codes ){
-    ls_bucket[[pc]] <- ds %>% 
-      dplyr::filter(palette_code == pc) %>% 
-      dplyr::select(-id, -gender, -age_group)
-  }
-  
-  d_roster <- ds %>% 
-    dplyr::group_by(id, palette_code, gender,age_group) %>%
-    # dplyr::group_by(id ) %>% 
-    dplyr::summarize(
-      n_encounters = length(unique(encounter_id))
-    )
-  ###
-  ls_person <- list()
-  dd <- d_roster %>% dplyr::filter(id == person_id) %>% as.data.frame() 
-  person_codes <- dd[,"palette_code"]
-  person_draws <- dd[,"n_encounters"]
-  person_gernder <- unique(dd[,"gender"])[1]
-  person_age_group <- unique(dd[,"age_group"])[1]
-  for(p in seq_along(person_codes)){
-    # p = 1
-    # encounters_in_this_code <- ls_bucket[[person_codes[p]]]# %>% dplyr::distinct(encounter_id)# %>% as.data.frame()
-    encounters_in_this_code <- ls_bucket[[person_codes[p]]] %>% as.data.frame()
-    encounters_in_this_code <- unique(encounters_in_this_code[,"encounter_id"])
-    # encounters_in_this_code <- encounters_in_this_code[,"encounter_id"]
-    select_encounters <- sample(encounters_in_this_code, person_draws[p], replace = FALSE)  
-    
-    ls_person[[person_codes[p]]] <- ls_bucket[[person_codes[p]]] %>% 
-      dplyr::filter(encounter_id %in% select_encounters)
-    d_person <- dplyr::bind_rows(ls_person)
-    d_person[,"id"] <- person_id
-    d_person[,"gender"] <- person_gernder
-    d_person[,"age_group"] <- person_age_group
-    d_person <- d_person %>% dplyr::select(id,gender, age_group, dplyr::everything())
-    
-  } 
-  return(d_person)
-}
-# Usage :
-# d <- ds %>% scramble_one(10042753)
+x <- ds %>% 
+  dplyr::select(id, gender, age_group, encounter_id, palette_colour_name)
 
-scramble_many <- function(ds, sample_size){
-  # sample_size = 10
-  ls_temp <- list()
-  select_persons <- sample(unique(ds$id), sample_size, replace = FALSE)
-  for(i in select_persons ){  
-    ls_temp[[i]] <- scramble_one(ds, i)
-  }
-  d <- ls_temp %>% dplyr::bind_rows()
-  return(d)
-}  
-
-set.seed(1)
-d <- ds %>% scramble_many(10)
-d <- d %>%
-  dplyr::mutate(
-    encounter_id = encounter_id + round(runif(1,1,10000),0)
+d_patients <- ds %>% 
+  dplyr::group_by(id, palette_code, palette_colour_name) %>%
+  # dplyr::group_by(id ) %>% 
+  dplyr::summarize(
+    n_encounters = length(unique(encounter_id)),
+    n_events     = sum(event_count) 
   )
-saveRDS(d, paste0(path_save,".rds"))
-readr::write_csv(d, paste0(path_save,".csv"))
+x <- d_patients
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+patient_pallete_codes %>% unique(d_patients[,"pa"])
+select_palette_code <- 
+filter_criteria_palette <- lazyeval::interp(~ which_column %in% select_palette_code,which_column = as.name("palette_code"))
+d <- ds %>% 
+  # create a bucket
+  dplyr::filter_(.dots = filter_criteria_palette )
+x <- d 
